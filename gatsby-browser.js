d67c3e4b1070d1f2e6dc8e86ff88390c542a0c74
Greg Lobinski <greglobinski@gmail.com>
2021-09-15 11:48:27 +0200

    use recomened

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -6,25 +6,25 @@
 
 // You can delete this file if you're not using it
 
-export const onInitialClientRender = () => {
-  const gtmScript = document.createElement("script")
-  gtmScript.src = "https://www.googletagmanager.com/gtm.js?id=GTM-KDHT88T"
-  gtmScript.onload = function () {
-    // init GTM
-    const dataLayer = window.dataLayer || []
-    dataLayer.push({ "gtm.start": new Date().getTime(), event: "gtm.js" })
-  }
-
-  document.head.appendChild(gtmScript)
-}
-
 // export const onInitialClientRender = () => {
 //   const gtmScript = document.createElement("script")
-//   gtmScript.type = "text/javascript"
-//   gtmScript.innerText = `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
-// new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
-// j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
-// 'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-// })(window,document,'script','dataLayer','GTM-KDHT88T');`
+//   gtmScript.src = "https://www.googletagmanager.com/gtm.js?id=GTM-KDHT88T"
+//   gtmScript.onload = function () {
+//     // init GTM
+//     const dataLayer = window.dataLayer || []
+//     dataLayer.push({ "gtm.start": new Date().getTime(), event: "gtm.js" })
+//   }
+
 //   document.head.appendChild(gtmScript)
 // }
+
+export const onInitialClientRender = () => {
+  const gtmScript = document.createElement("script")
+  gtmScript.type = "text/javascript"
+  gtmScript.innerText = `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+})(window,document,'script','dataLayer','GTM-KDHT88T');`
+  document.head.appendChild(gtmScript)
+}
